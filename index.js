aaef4dad21ec9dc5547a77142989d4d1351643bb
K2302 <95227135+K2302@users.noreply.github.com>
2024-10-29 17:32:45 +0530

    react installed

---
+++ index.js
@@ -0,0 +1,12 @@
+const { Server } = require("socket.io");
+
+const io = new Server(8000, {
+  cors: true,
+});
+
+const emailToSocketIdMap = new Map();
+const socketidToEmailMap = new Map();
+
+io.on("connection", (socket) => {
+  console.log(`Socket Connected`, socket.id);
+});
